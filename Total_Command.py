d18c68cd765955648d2cf23997f1b720c8a826be
1643s <shu.snowleopard1643@gmail.com>
2026-07-14 08:53:29 +0900

    Sinfonien4-3

--- Total_Command.py
+++ Total_Command.py
@@ -12,10 +12,10 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-midipath =  "/Users/shumiyajima/EEPYTHON/MIDI/midi/BACH_SINFONIEN/4 完遂/4-2.mid"
+midipath =  "/Users/shumiyajima/EEPYTHON/MIDI/midi/BACH_SINFONIEN/4 完遂/4-3.mid"
 csvfolder =  "/Users/shumiyajima/EEPYTHON/MIDI/midi/BACH_SINFONIEN/4 完遂"
-filename = "Bach_Sinfonien4_2"
-filename2 = "Bach-Sinfonien4-2"
+filename = "Bach_Sinfonien4_3"
+filename2 = "Bach-Sinfonien4-3"
 logpath = "/Users/shumiyajima/EEPYTHON/MIDI/midi/log.csv"
 
 #記録リストチェック
